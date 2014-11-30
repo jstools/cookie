b54a93e0cb05e785bf9983e3ed0034f5ea8a291c
Jesús Germade <jesus@germade.es>
2014-11-30 10:47:57 +0100

    renaming to empty

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-events
-======
+jstool-empty
+============
---
+++ empty.js
@@ -0,0 +1,7 @@
+fn.define('Empty', function () {
+	'use strict';
+
+	return function () {
+        console.log('this is just for demo purposes');
+    };
+});
